8ad419d71b69df9fa3bddb1a7635727060e78e46
Yagnik <yagniksram@gmail.com>
2024-06-26 20:40:01 +0530

    commit changes

--- 6.Random_Forest.py
+++ 6.Random_Forest.py
@@ -3,7 +3,7 @@
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.datasets import load_iris
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, classification_report
 
 # Load the iris dataset 
 iris = load_iris()
@@ -25,7 +25,21 @@ y_pred = rf_classifier.predict(X_test)
 
 # Calculate accuracy
 accuracy = accuracy_score(y_test, y_pred)
+report = classification_report(y_test, y_pred, target_names=iris.target_names)
+
 print("Accuracy:", accuracy)
+print('Classification Report:')
+print(report)
 
 # Output:
 # Accuracy: 1.0
+# Classification Report:
+#               precision    recall  f1-score   support
+
+#       setosa       1.00      1.00      1.00        19
+#   versicolor       1.00      1.00      1.00        13
+#    virginica       1.00      1.00      1.00        13
+
+#     accuracy                           1.00        45
+#    macro avg       1.00      1.00      1.00        45
+# weighted avg       1.00      1.00      1.00        45
